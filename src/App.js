09eaedaf674be421f88529a08f7b0a054b2be290
fabio loterio <vitorloterio@gmail.com>
2020-10-14 16:16:09 -0300

    project upload - finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+import "./mine.css";
+
+// exemplos. - - - - - - - - - - - - - - - - - - - - - - - -
+const elemento = (<div><h2>Fala galera</h2></div>); // com o jsx e abaixo o mesmo sem usar o JSX
+
+const elemento2 = React.createElement("div", null, React.createElement("h2", null, "Fala galera"));
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function App() {
+  // ENTRADA, RODANDO, FIM
+  const [estado, setEstado] = useState("ENTRADA");
+
+  // palpite
+  const [palpite, setPalpite] = useState(150);
+  const [numPalpites, setNumPalpites] = useState(1);
+
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(300);
+
+  const iniciarJogo = () => {
+    setEstado("RODANDO");
+    setPalpite(150);
+    setNumPalpites(1);
+    setMin(0);
+    setMax(300);
+  };
+  if (estado === "ENTRADA") {
+    return (
+      <button onClick={iniciarJogo} id="dot">
+        Iniciar Jogo
+      </button>
+    );
+  }
+
+  const menor = () => {
+    setNumPalpites(numPalpites + 1);
+    setMax(palpite);
+    const proxPalpite = parseInt((palpite - min) / 2) + min;
+    setPalpite(proxPalpite);
+  };
+
+  const maior = () => {
+    setNumPalpites(numPalpites + 1);
+    setMin(palpite);
+    const proxPalpite = parseInt((max - palpite) / 2) + palpite;
+    setPalpite(proxPalpite);
+  };
+
+  const acertou = () => {
+    setEstado("FIM");
+  };
+
+  if (estado === "FIM") {
+    return (
+      <div>
+        <p>
+          Acertei o número que você pensou({palpite}) com {numPalpites} chutes!
+        </p>
+        <button onClick={iniciarJogo} id="dot">
+          Jogar novamente
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>O seu número é {palpite}?</p>
+      <button onClick={menor}>Menor!</button>
+      <button onClick={acertou} id="yea">
+        Acertou!
+      </button>
+      <button onClick={maior}>Maior!</button>
     </div>
   );
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App name="bigo" />, rootElement);
+
+export default App
